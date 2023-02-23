5bce2b751cb1328df9b8e71979df74be3350c73b
김연구 <yeongoo.kim@qualson.com>
2023-02-23 22:59:47 +0900

    feat: Add screens email, login, signup

---
+++ lib/features/authentication/log_in_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  void _onSignupTap(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        elevation: 2,
+        color: Colors.grey.shade50,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size32,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have an account?"),
+              Gaps.h5,
+              GestureDetector(
+                onTap: () => _onSignupTap(context),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            children: const [
+              Gaps.v80,
+              Text(
+                'Log in to TikTok',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              Text(
+                "Manage your account, check notifications, comment on videos, and more",
+                style: TextStyle(
+                  fontSize: Sizes.size14,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Gaps.v40,
+              AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.user),
+                  text: "Use Email & password"),
+              Gaps.v16,
+              AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.apple),
+                  text: "Continue with Apple")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/sign_up_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/log_in_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  void _onLoginTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const LoginScreen(),
+    ));
+  }
+
+  void _onEmailTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const EmailScreen(),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        elevation: 2,
+        color: Colors.grey.shade50,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size32,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Already have an account?'),
+              Gaps.h5,
+              GestureDetector(
+                onTap: () => _onLoginTap(context),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            children: [
+              Gaps.v80,
+              const Text(
+                'Sign up for TikTok',
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v20,
+              const Text(
+                "Create a profile, follow other account, make your own videos, and more",
+                style: TextStyle(
+                  fontSize: Sizes.size14,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Gaps.v40,
+              AuthButton(
+                icon: const FaIcon(FontAwesomeIcons.user),
+                text: "Use Email & password",
+                onTap: () => _onEmailTap(context),
+              ),
+              Gaps.v16,
+              const AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.apple),
+                  text: "Continue with Apple")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
